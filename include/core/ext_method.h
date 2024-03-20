4e5e822d31f843e150fa744ee7091e8a4b917596
Dasaav-dsv <simplydasaav@gmail.com>
2024-03-20 11:53:08 +0100

    Add libER namespace

--- include/core/ext_method.h
+++ include/core/ext_method.h
@@ -2,22 +2,24 @@
 
 #include <type_traits>
 
-template <typename>
-union ExtMethodHolder;
+namespace libER {
+    template <typename>
+    union ExtMethodHolder;
 
-template <class Class, auto Function, typename = decltype(Function)>
-class ExtMethod;
+    template <class Class, auto Function, typename = decltype(Function)>
+    class ExtMethod;
 
-template <class Class, auto Function, typename Result, class ClassP, typename...Args>
-class ExtMethod<Class, Function, Result(*)(ClassP, Args...)> {
-    static_assert(std::is_same_v<const volatile Class, const volatile typename std::remove_pointer_t<ClassP>>,
-        "Bound function must take a pointer of its class type");
-    Class* instance;
-public:
-    auto operator()(Args...args) {
-        if constexpr (std::is_same_v<Result, void>)
-            Function(this->instance, args...);
-        else
-            return Function(this->instance, args...);
-    }
-};
+    template <class Class, auto Function, typename Result, class ClassP, typename...Args>
+    class ExtMethod<Class, Function, Result(*)(ClassP, Args...)> {
+        static_assert(std::is_same_v<const volatile Class, const volatile typename std::remove_pointer_t<ClassP>>,
+            "Bound function must take a pointer of its class type");
+        Class* instance;
+    public:
+        auto operator()(Args...args) {
+            if constexpr (std::is_same_v<Result, void>)
+                Function(this->instance, args...);
+            else
+                return Function(this->instance, args...);
+        }
+    };
+}
