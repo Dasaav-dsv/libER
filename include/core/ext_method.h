88192e2c6aea836e259a60e0010062b58a02b3e5
Dasaav-dsv <simplydasaav@gmail.com>
2024-03-20 11:51:14 +0100

    Add external method support

---
+++ include/core/ext_method.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <type_traits>
+
+template <typename>
+union ExtMethodHolder;
+
+template <class Class, auto Function, typename = decltype(Function)>
+class ExtMethod;
+
+template <class Class, auto Function, typename Result, class ClassP, typename...Args>
+class ExtMethod<Class, Function, Result(*)(ClassP, Args...)> {
+    static_assert(std::is_same_v<const volatile Class, const volatile typename std::remove_pointer_t<ClassP>>,
+        "Bound function must take a pointer of its class type");
+    Class* instance;
+public:
+    auto operator()(Args...args) {
+        if constexpr (std::is_same_v<Result, void>)
+            Function(this->instance, args...);
+        else
+            return Function(this->instance, args...);
+    }
+};
