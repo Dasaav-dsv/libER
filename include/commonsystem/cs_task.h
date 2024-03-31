da6a1473e1164030d99e1f47adccaf2b5922e153
Dasaav-dsv <simplydasaav@gmail.com>
2024-03-31 21:48:54 +0200

    Rearrange, import task groups

--- include/commonsystem/cs_task.h
+++ include/commonsystem/cs_task.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include <detail/liber_preprocessor.h>
+#include <detail/preprocessor.h>
 #include <memory/from_allocator.h>
 #include <memory/from_unique_ptr.h>
 #include <memory/from_vector.h>
 #include <fd4/component.h>
+#include <fd4/singleton.h>
 #include <fd4/fd4_task.h>
 
+#include <span>
 #include <string>
 #include <algorithm>
+
+// TODO: annotate
 
 #define CS_TASK_GROUP_ID(INDEX) (INDEX ^ LIBER_BIT_FLAG(28) ^ LIBER_BIT_FLAG(31))
 
@@ -18,15 +22,6 @@ namespace from {
 
         // CSTaskGroupId
         using cstgi = unsigned int;
-
-        struct cstg_descriptor {
-            cstg_descriptor(cstgi id, bool active)
-                : id(id), active(active) {}
-
-            cstgi id;
-            wchar_t name[40]{};
-            bool active;
-        };
 
         class CSEzTaskProxy : public FD4::FD4TaskBase {
         public:
@@ -59,17 +54,28 @@ namespace from {
         };
 
         class CSTaskBase {
+            struct cstg_control {
+                cstg_control(cstgi id, bool active)
+                    : id(id), active(active) {}
+
+                cstgi id;
+                wchar_t name[40]{};
+                bool active;
+            };
+
         public:
             virtual ~CSTaskBase() LIBER_INTERFACE_ONLY;
 
             LIBER_UNKNOWN(from::allocator<void>);
-            from::vector<cstg_descriptor> task_groups;
+            from::vector<cstg_control> task_groups;
             cstgi task_group_count;
         };
 
         // Not constructible, exposition only
         class CSTask : private CSTaskBase {
         public:
+            FD4_SINGLETON_CLASS(CSTask);
+
             virtual ~CSTask() LIBER_INTERFACE_ONLY;
             virtual void add_task_group(const wchar_t* name) LIBER_INTERFACE_ONLY;
             virtual bool unk_set_task_group(cstgi) LIBER_INTERFACE_ONLY;
@@ -84,5 +90,25 @@ namespace from {
             void* task_seeds_0x60[6];
             LIBER_UNKNOWN(bool);
         };
+
+        class CSTaskGroup {
+            struct cstg_locator {
+                int id;
+                wchar_t* name;
+                LIBER_UNK_ARR(int, 4);
+            };
+
+        public:
+            FD4_SINGLETON_CLASS(CSTaskGroup);
+
+            virtual ~CSTaskGroup() LIBER_INTERFACE_ONLY;
+
+            #include "taskgroups.inl"
+            void* task_groups[TaskGroups::SIZE];
+        };
+
+        LIBER_ASSERTS_BEGIN(CSTask);
+        LIBER_ASSERT_SIZE(0xE8);
+        LIBER_ASSERTS_END;
     }
 }
