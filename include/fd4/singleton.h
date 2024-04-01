6348ab0ddb4fae4d826a069cf521a8480910165b
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-01 13:29:11 +0200

    May as well be [[nodiscard]]

--- include/fd4/singleton.h
+++ include/fd4/singleton.h
@@ -3,6 +3,6 @@
 #include <detail/preprocessor.h>
 #include <detail/optref.h>
 
-#define FD4_SINGLETON_CLASS(CLASSNAME)               \
-static liber::optref<CLASSNAME> instance() noexcept; \
+#define FD4_SINGLETON_CLASS(CLASSNAME)                             \
+[[nodiscard]] static liber::optref<CLASSNAME> instance() noexcept; \
 LIBER_INTERFACE_CLASS(CLASSNAME)
