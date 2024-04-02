551b3ed2f86e4a3cde71b87038ed8ad2b93097d6
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-02 12:04:29 +0200

    Comment for wait_for_system

--- include/dantelion2/system.h
+++ include/dantelion2/system.h
@@ -6,18 +6,26 @@
 namespace from {
     // Dantelion system
     namespace DLSY {
-        // TODO: desc
-        // DO NOT CALL FROM DLLMAIN - DEADLOCK!
+        // Lock until main ELDEN RING system resources
+        // are initialized, or time runs out. Initialization
+        // is performed by the game's main thread, post-entry.
+        // Returns true on success, false on timeout.
+        // Timeout can be -1 (indefinite wait)
+        //
+        // DO NOT call this function from a thread
+        // that the game's main thread may wait on.
+        // ^^^ POTENTIAL DEADLOCK vvv
+        // DO NOT CALL IT FROM DLLMAIN
+        //
         bool wait_for_system(int timeout) noexcept;
 
+        // A singleton that deals with locale
+        // and stdio. Seldom used.
         class DLRuntimeImpl {
         public:
             FD4_SINGLETON_CLASS(DLRuntimeImpl);
 
             virtual ~DLRuntimeImpl() LIBER_INTERFACE_ONLY;
-
-        private:
-            void* liber_unknown[11];
         };
     }
 }
