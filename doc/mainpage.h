a2e4dd497150d4bf0c0d63a712c7d995a484301d
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-12 10:47:58 +0200

    Begin drafting the main page

---
+++ doc/mainpage.h
@@ -0,0 +1,15 @@
+/**
+ * @mainpage libER reference
+ *
+ * @section about_section About
+ *
+ * libER is an ELDEN RING Application Programming Interface (API) library that
+ * aims to provide a safe C++ interface for the game.
+ *
+ * @see https://github.com/Dasaav-dsv/libER
+ *
+ * @section explore_section Explore
+ * 
+ * @link @endlink
+ * 
+ */
