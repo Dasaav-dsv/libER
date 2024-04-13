a17cf31408b4fd26af19f1ed9e2e84f9a09932c7
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-13 15:11:03 +0200

    Separate target_version from call

--- source/version/updater/version_updater.cpp
+++ source/version/updater/version_updater.cpp
@@ -1,10 +1,13 @@
 #include <version/detail/version_winhttp.hpp>
 #include "Windows.h"
 
+// Simple executable that downloads symbol .csvs from GitHub
+// and saves them to %APPDATA%/EldenRing/libER/[target_version]
 int main() {
     int argc = 0;
     LPWSTR cmdline = GetCommandLineW();
     LPWSTR* argv_w = CommandLineToArgvW(cmdline, &argc);
     if (argc <= 1) return 1;
-    return !liber::download_versioned_csv(argv_w[1]);
+    LPWSTR target_version = argv_w[1];
+    return !liber::download_versioned_csv(target_version);
 }
