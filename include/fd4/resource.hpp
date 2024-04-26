21be63897cabf5bcca674cff12c8a02a161d8533
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-26 12:46:15 +0200

    Begin annotating for Doxygen

--- include/fd4/resource.hpp
+++ include/fd4/resource.hpp
@@ -15,16 +15,18 @@
 
 #include <filesystem>
 
-// TODO: anchor vtables, doxygen documentation
-/// @cond DOXYGEN_SKIP
+// TODO: anchor vtables
 
 namespace from {
 namespace FD4 {
-
 // Forward declarations
 class FD4FileCap;
 class FD4ResRep;
 
+/**
+ * @brief Structure internal to the file processing in ELDEN RING.
+ *
+ */
 class FD4FileLoadProcess {
 public:
     virtual ~FD4FileLoadProcess() = default;
@@ -36,32 +38,68 @@ private:
     DLUT::DLReferenceCountPtr<DLUT::dummy_rfco> liber_unknown;
 };
 
+/**
+ * @brief A single abstract item in a resource hash table.
+ *
+ * Can be a resource, file or even a repository.
+ *
+ */
 class FD4ResCapHolderItem : public FD4ComponentBase {
 public:
     FD4_RUNTIME_CLASS(FD4ResCapHolderItem);
 
     ~FD4ResCapHolderItem() = default;
 
+    /**
+     * @brief Get the item name.
+     *
+     * @return DLTX::FD4BasicHashString& name
+     */
     DLTX::FD4BasicHashString& name() {
         return this->res_name;
     }
 
+    /**
+     * @brief Get the item name (const).
+     *
+     * @return DLTX::FD4BasicHashString& name
+     */
     const DLTX::FD4BasicHashString& name() const {
         return this->res_name;
     }
 
+    /**
+     * @brief Get the next item in the hash table.
+     *
+     * @return FD4ResCapHolderItem* next item (may be null)
+     */
     FD4ResCapHolderItem* next() {
         return this->next_item;
     }
 
+    /**
+     * @brief Get the next item in the hash table (const).
+     *
+     * @return FD4ResCapHolderItem* next item (may be null)
+     */
     const FD4ResCapHolderItem* next() const {
         return this->next_item;
     }
 
+    /**
+     * @brief Get the reference count of the item.
+     *
+     * @return size_t&
+     */
     size_t& ref_count() {
         return this->rcount;
     }
 
+    /**
+     * @brief Get the reference count of the item (const).
+     *
+     * @return size_t&
+     */
     const size_t& ref_count() const {
         return this->rcount;
     }
@@ -73,14 +111,27 @@ private:
     size_t rcount;
 };
 
-// Resource capsule base
+/**
+ * @brief The base of any resource, which can also be a file or a repository.
+ *
+ * Only differs from FD4::FD4ResCapHolderItem in having some debug members.
+ * 
+ */
 class FD4ResCap : public FD4ResCapHolderItem {
 public:
     FD4_RUNTIME_CLASS(FD4ResCap);
 
     ~FD4ResCap() = default;
 
+    /**
+     * @brief Set the internal debugging state.
+     * 
+     * @note libER does not document this ELDEN RING debug property.
+     * 
+     * @param value on/off
+     */
     virtual void set_debug(bool value) LIBER_INTERFACE;
+
     virtual void liber_unknown() LIBER_INTERFACE;
 
 private:
@@ -90,12 +141,17 @@ private:
     bool liber_unknown;
 };
 
+/**
+ * @brief A hash table of FD4::FD4ResCapHolderItems.
+ * 
+ */
 class FD4ResCapHolder : public FD4ComponentBase {
 public:
     FD4_RUNTIME_CLASS(FD4ResCapHolder);
 
     virtual ~FD4ResCapHolder() = default;
 
+private:
     virtual from::allocator<void> get_allocator1() const noexcept {
         return this->allocator;
     }
@@ -108,15 +164,32 @@ public:
         return this->allocator;
     }
 
+public:
+    /**
+     * @brief Get the owning repository of this capsule holder.
+     * 
+     * @return FD4ResRep* pointer to the owner
+     */
     virtual FD4ResRep* get_owner() noexcept {
         return this->owning_repository;
     }
 
+    /**
+     * @brief Get the owning repository of this capsule holder (const).
+     * 
+     * @return FD4ResRep* pointer to the owner
+     */
     virtual FD4ResRep* get_owner() const noexcept {
         return this->owning_repository;
     }
 
-    LIBERAPI FD4ResCap* get_res_cap(DLTX::string_hash* res_name) const;
+    /**
+     * @brief Get the capsule with the given name.
+     * 
+     * @param name resource name
+     * @return FD4ResCap* pointer to the capsule (may be null) 
+     */
+    LIBERAPI FD4ResCap* get_res_cap(const std::wstring& name) const;
 
 private:
     friend class FD4FileCapHolder;
@@ -128,13 +201,29 @@ private:
     FD4ResCapHolderItem** items;
 };
 
+/**
+ * @brief A hash table of FD4::FD4FileCaps.
+ * 
+ * Used by file repositories.
+ * 
+ */
 class FD4FileCapHolder : public FD4ResCapHolder {
 public:
     FD4_RUNTIME_CLASS(FD4FileCapHolder);
 
+    /**
+     * @brief Get the file capsule with the given filename.
+     * 
+     * @param name path
+     * @return FD4FileCap* pointer to the capsule (may be null) 
+     */
     LIBERAPI FD4FileCap* get_file_cap(const std::filesystem::path& path) const;
 };
 
+/**
+ * @brief States of file capsule processing.
+ * 
+ */
 enum class FD4FileCapState : char {
     INITIAL = 0,
     QUEUED = 1,
@@ -143,20 +232,40 @@ enum class FD4FileCapState : char {
     READY = 4
 };
 
+/**
+ * @brief Abstract file capsule class.
+ * 
+ */
 class FD4FileCap : public FD4ResCap {
 public:
     FD4_RUNTIME_CLASS(FD4FileCap);
 
     virtual ~FD4FileCap() = default;
 
+    /**
+     * @brief Get the processing state.
+     * 
+     * @return FD4FileCapState 
+     */
     FD4FileCapState get_state() const noexcept {
         return this->state;
     }
 
+    /**
+     * @brief Get the load task associated with the capsule.
+     * 
+     * @return void* 
+     */
     void* get_load_task() const noexcept {
         return this->load_task;
     };
 
+    /**
+     * @brief Has the file been fully processed?
+     * 
+     * @return true 
+     * @return false 
+     */
     bool ready() const noexcept {
         return this->state == FD4FileCapState::READY;
     }
@@ -169,7 +278,19 @@ private:
     virtual void* liber_unknown() = 0;
 
 public:
+    /**
+     * @brief Request a file to be loaded with the specified load process.
+     * 
+     * @param load_process the load process
+     */
     virtual void request_file_load(FD4FileLoadProcess* load_process) = 0;
+
+    /**
+     * @brief Force the loading of the file capsule's associated resources.
+     * 
+     * @note Does not work for all file capsule types.
+     * 
+     */
     virtual void force_file_load() = 0;
 
 private:
@@ -185,6 +306,7 @@ private:
     short liber_unknown;
 };
 
+// TODO: FD4FileRep for files
 class FD4ResRep : public FD4ResCap {
 public:
     FD4_RUNTIME_CLASS(FD4ResRep);
@@ -252,6 +374,5 @@ LIBER_ASSERTS_END;
 LIBER_ASSERTS_BEGIN(FD4ResManagerImp);
 LIBER_ASSERT_SIZE(0x158);
 LIBER_ASSERTS_END;
-/// @endcond
 } // namespace FD4
 } // namespace from
