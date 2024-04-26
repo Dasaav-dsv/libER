67d2ea902febb471f65f8290a9fb016a6c2732ee
Dasaav-dsv <simplydasaav@gmail.com>
2024-04-26 07:40:07 +0200

    CSFile and file loading

--- include/coresystem/file/file.hpp
+++ include/coresystem/file/file.hpp
@@ -7,13 +7,17 @@
  */
 #pragma once
 
+#include <coresystem/task.hpp>
 #include <detail/optref.hpp>
 #include <detail/preprocessor.hpp>
 #include <fd4/detail/singleton.hpp>
 #include <fd4/resource.hpp>
+#include <memory/from_shared_ptr.hpp>
 #include <memory/from_unique_ptr.hpp>
 
+#include <atomic>
 #include <filesystem>
+#include <memory>
 #include <string>
 
 namespace from {
@@ -28,12 +32,153 @@ class CSFileImp {
 public:
     FD4_SINGLETON_CLASS(CSFileImp);
 
-    [[nodiscard]] LIBERAPI from::unique_ptr<FD4::FD4FileCap> load_resource_file(
-        const std::filesystem::path& file);
+    virtual ~CSFileImp() LIBER_INTERFACE_ONLY;
 
-    LIBERAPI liber::optref<FD4::FD4ResCap> get_resource(
-        CSResourceRepository repository, const std::wstring& resource,
-        const std::filesystem::path& file = std::filesystem::path{});
+    FD4::FD4ResRep* get_file_repository() noexcept {
+        return this->file_repository;
+    }
+
+private:
+    FD4::FD4ResRep* file_repository;
+};
+
+class CSFD4MoWwisebankRepositoryImp : public FD4::FD4ResRep {
+public:
+    FD4_SINGLETON_CLASS(CSFD4MoWwisebankRepositoryImp);
+
+    virtual ~CSFD4MoWwisebankRepositoryImp() LIBER_INTERFACE_ONLY;
 };
 } // namespace CS
+
+class file_request {
+public:
+    enum file_request_type : char {
+        LOAD = 0,
+        RELOAD = 1,
+        UNLOAD = 2
+    };
+
+    file_request(const std::filesystem::path& file, file_request_type type)
+        : path(file), type(type) {
+        this->init_request();
+    }
+
+    file_request(std::filesystem::path&& file, file_request_type type)
+        : path(file), type(type) {
+        this->init_request();
+    }
+
+    file_request(const file_request&) = delete;
+    file_request(file_request&&) noexcept = delete;
+
+    file_request& operator=(const file_request&) = delete;
+    file_request& operator=(file_request&&) noexcept = delete;
+
+    ~file_request() noexcept {
+        this->is_ready.test_and_set();
+        this->is_ready.notify_all();
+    }
+
+    bool ready() const noexcept {
+        return is_ready.test();
+    }
+
+    liber::optref<FD4::FD4FileCap> get() const noexcept {
+        this->is_ready.wait(false);
+        return this->check();
+    }
+
+    liber::optref<FD4::FD4FileCap> check() const noexcept {
+        if (!this->file_cap)
+            return std::nullopt;
+        return *this->file_cap;
+    }
+
+    using loader_type = FD4::FD4FileCap* (*)(CS::CSFileImp*, const wchar_t*,
+        void*, void*, void*, void*);
+
+private:
+    LIBERAPI void init_request();
+
+    std::filesystem::path path;
+    file_request_type type;
+    bool is_vfs_path = false;
+    bool unload_file = false;
+    enum class file_extension_type : char {
+        ANY = 0,
+        BNK = 1,
+        WEM = 2
+    } file_extension = file_extension_type::ANY;
+    loader_type loader = nullptr;
+    FD4::FD4FileCap* file_cap = nullptr;
+    mutable std::atomic_flag is_ready = ATOMIC_FLAG_INIT;
+    struct file_request_task : public CS::CSEzTask {
+        file_request_task(file_request& request) : request(request) {}
+        void eztask_execute(FD4::FD4TaskData*) override;
+        file_request& request;
+    };
+    from::unique_ptr<file_request_task> task =
+        from::make_unique<file_request_task>(*this);
+};
+
+class resource_request {
+public:
+    resource_request(CS::CSResourceRepository repository,
+        const std::wstring& resource,
+        const std::filesystem::path& source = std::filesystem::path{})
+        : repository(repository), resource(resource),
+          file(!source.empty() ? new file_request(source, file_request::LOAD)
+                               : nullptr) {
+        this->task->register_task(CS::CSTaskGroup::FileStepUpdate_End);
+    }
+
+    resource_request(CS::CSResourceRepository repository,
+        std::wstring&& resource,
+        const std::filesystem::path& source = std::filesystem::path{})
+        : repository(repository), resource(resource),
+          file(!source.empty() ? new file_request(source, file_request::LOAD)
+                               : nullptr) {
+        this->task->register_task(CS::CSTaskGroup::FileStepUpdate_End);
+    }
+
+    resource_request(const resource_request&) = delete;
+    resource_request(resource_request&&) noexcept = delete;
+
+    resource_request& operator=(const resource_request&) = delete;
+    resource_request& operator=(resource_request&&) noexcept = delete;
+
+    ~resource_request() noexcept {
+        this->is_ready.test_and_set();
+        this->is_ready.notify_all();
+    }
+
+    bool ready() const noexcept {
+        return is_ready.test();
+    }
+
+    liber::optref<FD4::FD4ResCap> get() const noexcept {
+        this->is_ready.wait(false);
+        return this->check();
+    }
+
+    liber::optref<FD4::FD4ResCap> check() const noexcept {
+        if (!this->res_cap)
+            return std::nullopt;
+        return *this->res_cap;
+    }
+
+private:
+    std::unique_ptr<file_request> file;
+    CS::CSResourceRepository repository;
+    std::wstring resource;
+    FD4::FD4ResCap* res_cap = nullptr;
+    mutable std::atomic_flag is_ready = ATOMIC_FLAG_INIT;
+    struct resource_request_task : public CS::CSEzTask {
+        resource_request_task(resource_request& request) : request(request) {}
+        void eztask_execute(FD4::FD4TaskData*) override;
+        resource_request& request;
+    };
+    from::unique_ptr<resource_request_task> task =
+        from::make_unique<resource_request_task>(*this);
+};
 } // namespace from
--- source/coresystem/file.cpp
+++ source/coresystem/file.cpp
@@ -1,29 +1,29 @@
 #include <coresystem/file/file.hpp>
+#include <dantelion2/fileio.hpp>
+#include <dantelion2/kernel_runtime.hpp>
+#include <detail/functions.hpp>
 #include <detail/literal_string.hpp>
 #include <detail/singleton.hpp>
-#include <detail/symbols.hpp>
 
-#include <cstddef>
-#include <cstdint>
+#include <algorithm>
 #include <unordered_map>
 
-using namespace from;
+#include "file_wwise.inl"
 
+using namespace from;
 namespace fs = std::filesystem;
 
 LIBER_SINGLETON_INSTANCE(CS::CSFileImp);
+LIBER_SINGLETON_INSTANCE(CS::CSFD4MoWwisebankRepositoryImp);
 
-using make_file_cap = FD4::FD4FileCap* (*)(CS::CSFileImp*, const wchar_t*,
-    size_t, size_t, size_t, size_t);
-
-#define LIBER_RESOURCE_EXTENSION(EXT)                                  \
-    { fs::path(LIBER_STRINGIFY(.##EXT)),                               \
-        reinterpret_cast<make_file_cap>(liber::symbol<LIBER_STRINGIFY( \
-                CS::CSFile::make_##EXT##_file_cap)>::get()) },
+#define LIBER_RESOURCE_EXTENSION(EXT)    \
+    { fs::path(LIBER_STRINGIFY(.##EXT)), \
+        liber::symbol<LIBER_STRINGIFY(   \
+            CS::CSFile::make_##EXT##_file_cap)>::get() },
 
 const auto& get_extension_map() {
     // clang-format off
-    static std::unordered_map<fs::path, make_file_cap> extension_map = {
+    static std::unordered_map<fs::path, void*> extension_map = {
 #include <coresystem/file/resource_extensions.inl>
     };
     // clang-format on
@@ -32,18 +32,133 @@ const auto& get_extension_map() {
 
 #undef LIBER_RESOURCE_EXTENSION
 
-from::unique_ptr<FD4::FD4FileCap> CS::CSFileImp::load_resource_file(
-    const fs::path& file) {
+void file_request::init_request() {
+    if (this->type != file_request::LOAD)
+        this->unload_file = true;
+    auto file_manager = DLIO::DLFileDeviceManager::instance();
+    this->is_vfs_path = file_manager.reference().is_virtual_path(this->path);
+    if (!this->is_vfs_path) {
+        this->path = std::filesystem::absolute(this->path);
+        if (!fs::exists(this->path))
+            throw fs::filesystem_error("file does not exist", this->path,
+                std::make_error_code(std::errc::no_such_file_or_directory));
+    }
     const auto& extension_map = get_extension_map();
-    auto iter = extension_map.find(file.extension());
-    if (iter == extension_map.end())
-        return nullptr;
-    return from::unique_ptr<FD4::FD4FileCap>{ iter->second(this,
-        file.wstring().c_str(), 0, 0, 0, 0) };
-    // Don't actually force the file loading!
-    // It may interrupt the load process of more complex resources.
-    // file_cap->force_file_load();
+    fs::path extension = this->path.extension();
+    if (extension == L".dcx")
+        this->path.replace_extension();
+    extension = this->path.extension();
+    auto iter = extension_map.find(extension);
+    if (iter != extension_map.end())
+        this->loader = (file_request::loader_type)iter->second;
+    else if (!extension.compare(L".bnk"))
+        this->file_extension = file_extension_type::BNK;
+    else if (!extension.compare(L".wem"))
+        this->file_extension = file_extension_type::WEM;
+    else
+        throw fs::filesystem_error("unsupported file format", extension,
+            std::make_error_code(std::errc::not_supported));
+    if (this->file_extension == file_extension_type::WEM) {
+        auto& [overrides, mutex] = wem_overrides();
+        std::unique_lock lock{ mutex };
+        switch (this->type) {
+        case file_request::LOAD:
+            overrides.emplace(path.filename(), path);
+            break;
+        case file_request::RELOAD:
+            overrides.insert_or_assign(path.filename(), path);
+            break;
+        case file_request::UNLOAD:
+            overrides.erase(path.filename());
+            break;
+        }
+    }
+    this->task->register_task(CS::CSTaskGroup::FileStepUpdate_Begin);
 }
+
+liber::optref<FD4::FD4ResRep> get_file_repository(bool load_bnk) {
+    if (load_bnk)
+        return CS::CSFD4MoWwisebankRepositoryImp::instance();
+    auto csfile = CS::CSFileImp::instance();
+    if (!csfile.has_reference())
+        return std::nullopt;
+    return *csfile.reference().get_file_repository();
+}
+
+void load_file_cap(const fs::path& path, file_request::loader_type loader,
+    bool load_bnk) {
+    if (!load_bnk) {
+        FD4::FD4FileCap* file_cap =
+            loader(&CS::CSFileImp::instance().reference(), path.c_str(),
+                nullptr, nullptr, nullptr, nullptr);
+        file_cap->ref_count() = 0x4000'0000;
+        return;
+    }
+    fs::path file = path.filename();
+    file.replace_extension();
+    FD4::FD4FileCap* file_cap =
+        liber::function<"CS::CSFD4MoWwisebankRepositoryImp::load_soundbank",
+            FD4::FD4FileCap*>::
+            call(&CS::CSFD4MoWwisebankRepositoryImp::instance().reference(),
+                file.c_str(), file.c_str(), false);
+    file_cap->ref_count() = 0x4000'0000;
+    auto& [overrides, mutex] = bnk_overrides();
+    std::unique_lock lock{ mutex };
+    overrides.insert_or_assign(path.filename(), path);
+}
+
+void file_request::file_request_task::eztask_execute(FD4::FD4TaskData*) {
+    file_request& request = this->request;
+    if (request.ready())
+        return;
+    bool load_bnk = request.file_extension == file_extension_type::BNK;
+    bool load_wem = request.file_extension == file_extension_type::WEM;
+    if (load_bnk || load_wem) {
+        if (!is_init_loaded())
+            return;
+        override_wwise_loader_once();
+        if (load_wem) {
+            request.is_ready.test_and_set();
+            request.is_ready.notify_all();
+            request.task.reset();
+            return;
+        }
+    }
+    auto file_rep_opt = get_file_repository(load_bnk);
+    if (!file_rep_opt.has_reference())
+        return;
+    FD4::FD4FileCapHolder& holder = file_rep_opt.reference().get_file_holder();
+    FD4::FD4FileCap* existing = nullptr;
+    if (load_bnk)
+        existing = holder.get_file_cap(request.path.stem());
+    else
+        existing = holder.get_file_cap(request.path);
+    if (request.unload_file) {
+        if (existing)
+            std::destroy_at(existing);
+        existing = nullptr;
+        request.unload_file = false;
+    }
+    switch (request.type) {
+    case file_request::LOAD:
+    case file_request::RELOAD:
+        if (!existing) {
+            load_file_cap(request.path, request.loader, load_bnk);
+            return;
+        }
+        if (!load_bnk && !existing->ready()
+            || load_bnk && !is_bank_ready(existing))
+            return;
+        break;
+    case file_request::UNLOAD:
+        existing = nullptr;
+        break;
+    }
+    request.file_cap = existing;
+    request.is_ready.test_and_set();
+    request.is_ready.notify_all();
+    request.task.reset();
+};
 
 #define LIBER_RESOURCE_REPOSITORY(NAME)                                        \
     case CS::CSResourceRepository::NAME:                                       \
@@ -51,29 +166,28 @@ from::unique_ptr<FD4::FD4FileCap> CS::CSFileImp::load_resource_file(
             liber::symbol<LIBER_STRINGIFY(CS::##NAME##Imp::instance)>::get()); \
         break;
 
-liber::optref<FD4::FD4ResCap> CS::CSFileImp::get_resource(
-    CS::CSResourceRepository repository, const std::wstring& resource,
-    const fs::path& file) {
-    auto string_hash = DLTX::string_hash{ resource.c_str() };
+void from::resource_request::resource_request_task::eztask_execute(
+    FD4::FD4TaskData*) {
+    resource_request& request = this->request;
     uintptr_t res_repository = 0;
-    switch (repository) {
+    switch (request.repository) {
 #include <coresystem/file/resource_repositories.inl>
     };
     if (!res_repository)
-        return std::nullopt;
-    int holder_offset = 0x78;
-    switch (repository) {
+        return;
+    size_t holder_offset = 0x78;
+    switch (request.repository) {
     case CS::CSResourceRepository::CSGparamRepository:
         holder_offset += 0x18;
+        break;
     }
     FD4::FD4ResCapHolder* res_cap_holder =
         reinterpret_cast<FD4::FD4ResCapHolder*>(res_repository + holder_offset);
-    FD4::FD4ResCap* result = res_cap_holder->get_res_cap(&string_hash);
-    if (result)
-        return *result;
-    if (!file.empty())
-        this->load_resource_file(file);
-    return std::nullopt;
+    auto hash = DLTX::string_hash{ request.resource.c_str() };
+    request.res_cap = res_cap_holder->get_res_cap(&hash);
+    request.is_ready.test_and_set();
+    request.is_ready.notify_all();
+    request.task.reset();
 }
 
 #undef LIBER_RESOURCE_REPOSITORY
