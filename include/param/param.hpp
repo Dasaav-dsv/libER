07717eb5f5e9d55698bb93779f42440e324e2f64
Dasaav-dsv <simplydasaav@gmail.com>
2024-05-10 12:53:18 +0200

    Add initial param tables

--- include/param/param.hpp
+++ include/param/param.hpp
@@ -1,17 +1,18 @@
 /**
  * @file param.hpp
  * @brief ELDEN RING param manipulation
- * 
+ *
  * Copyright 2024 libER ELDEN RING API library
- * 
+ *
  */
 #pragma once
 
+#include <detail/preprocessor.hpp>
 #include <param/detail/paramdef.hpp>
 
 namespace from {
 namespace param {
-#define LIBER_PARAM_ENTRY(PARAM, PARAMDEF) PARAM,  
+#define LIBER_PARAM_ENTRY(PARAM, PARAMDEF) PARAM,
 
 enum class param_index : int {
 #include <param/detail/paramlist.inl>
@@ -19,5 +20,24 @@ enum class param_index : int {
 };
 
 #undef LIBER_PARAM_ENTRY
-}
-}
+
+template <param_index Index, typename Def>
+class param_table {
+public:
+    using paramdef_type = Def;
+    static constexpr param_index index = Index;
+
+    constexpr param_table() = default;
+
+private:
+    LIBERAPI paramdef_type* get_row(int pos) const;
+};
+
+#define LIBER_PARAM_ENTRY(PARAM, PARAMDEF) \
+    inline constexpr param_table<param_index::PARAM, paramdef::PARAMDEF> PARAM;
+
+#include <param/detail/paramlist.inl>
+
+#undef LIBER_PARAM_ENTRY
+} // namespace param
+} // namespace from
