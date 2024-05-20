a522341c196c4c87aef19cd12b74457696c06fb9
Dasaav-dsv <simplydasaav@gmail.com>
2024-05-20 17:49:58 +0200

    Anchor vtables, add file description

--- source/fd4/resource.cpp
+++ source/fd4/resource.cpp
@@ -2,12 +2,13 @@
 #include <fd4/resource.hpp>
 
 using namespace from;
+using namespace from::FD4;
 
-FD4::FD4ResCap* FD4::FD4ResCapHolder::get_res_cap(
+FD4ResCap* FD4ResCapHolder::get_res_cap(
     const std::wstring& name) const {
     DLTX::string_hash hash{ name.c_str() };
     return liber::function<"FD4::FD4ResCapHolder::get_res_cap",
-        FD4::FD4ResCap*>::call(this, &hash);
+        FD4ResCap*>::call(this, &hash);
 }
 
 bool match_filename(const DLTX::FD4BasicHashString& hashstr,
@@ -21,16 +22,23 @@ bool match_filename(const DLTX::FD4BasicHashString& hashstr,
     return wsv == rhs;
 }
 
-FD4::FD4FileCap* FD4::FD4FileCapHolder::get_file_cap(
+FD4FileCap* FD4FileCapHolder::get_file_cap(
     const std::filesystem::path& path) const {
     std::wstring filename = path.filename().wstring();
     for (int i = 0; i < this->capacity; ++i) {
-        FD4::FD4ResCapHolderItem* item = this->items[i];
+        FD4ResCapHolderItem* item = this->items[i];
         while (item) {
             if (match_filename(item->name(), filename))
-                return reinterpret_cast<FD4::FD4FileCap*>(item);
+                return reinterpret_cast<FD4FileCap*>(item);
             item = item->next();
         }
     }
     return nullptr;
 }
+
+// Anchored vtables:
+FD4FileLoadProcess::~FD4FileLoadProcess() = default;
+FD4ResCapHolderItem::~FD4ResCapHolderItem() = default;
+FD4ResCap::~FD4ResCap() = default;
+FD4ResCapHolder::~FD4ResCapHolder() = default;
+FD4ResRep::~FD4ResRep() = default;
