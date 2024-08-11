cb32c78d3aca9922eb44be68e44a005f8af1853d
Dasaav-dsv <simplydasaav@gmail.com>
2024-08-11 21:06:23 +0200

    Update base_address for speed

--- include/detail/base_address.hpp
+++ include/detail/base_address.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
-/*
- * Functions to retrieve the process base address, caching it
- */
+#include <detail/defines.hpp>
+
+#include <cstddef>
+#include <cstdint>
 
 namespace liber {
-// Get the process base address
-void* base_address() noexcept;
+class _base_address {
+public:
+    LIBERAPI _base_address() noexcept;
 
-// Get an offset pointer from the process base address
-void* base_offset(int offset) noexcept;
+    void* get() const noexcept {
+        return offset(0);
+    }
+
+    void* offset(uintptr_t offset) const noexcept {
+        return reinterpret_cast<void*>(base + offset);
+    }
+
+private:
+    const uintptr_t base;
+};
+
+LIBERAPI extern _base_address base_address;
 } // namespace liber
--- source/detail/base_address.cpp
+++ source/detail/base_address.cpp
@@ -3,22 +3,7 @@
 
 using namespace liber;
 
-static void* cached_base_address;
+_base_address::_base_address() noexcept
+    : base(reinterpret_cast<uintptr_t>(GetModuleHandleW(NULL))) {}
 
-// There is no need for synchronization as
-// GetModuleHandle is cheap and thread-safe
-void* liber::base_address() noexcept {
-    void* result = cached_base_address;
-    if (!result) [[unlikely]] {
-        // GetModuleHandle(NULL) returns a pseudohandle
-        // (which is a module's base address) of the calling process
-        result = (void*)GetModuleHandle(NULL);
-        cached_base_address = result;
-    }
-    return result;
-}
-
-void* liber::base_offset(int offset) noexcept {
-    void* base = base_address();
-    return (void*)(reinterpret_cast<char*>(base) + offset);
-}
+_base_address liber::base_address;
